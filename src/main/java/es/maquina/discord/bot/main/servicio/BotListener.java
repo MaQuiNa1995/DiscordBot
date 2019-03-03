225f577f4a0aa7180b43261ae9e8ded89454ee08
MaQuiNa1995 <maquina1995@gmail.com>
2019-03-03 02:17:03 +0100

    añadido reglas, mapas aleatorios y jugadores por defecto

--- src/main/java/es/maquina/discord/bot/main/servicio/BotListener.java
+++ src/main/java/es/maquina/discord/bot/main/servicio/BotListener.java
@@ -1,6 +1,7 @@
 package es.maquina.discord.bot.main.servicio;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -38,7 +39,47 @@ public class BotListener extends ListenerAdapter {
 			canal.sendMessage(listarJugadores()).queue();
 			canal.sendMessage(montarEquipos()).queue();
 
+		} else if (mensajeUsuario.startsWith("!Reglas")) {
+			canal.sendMessage(crearAviso(canal)).queue();
+		} else if (mensajeUsuario.startsWith("!RifarMapa")) {
+			canal.sendMessage("Mapa Elegido: " + mapaAleatorio()).queue();
 		}
+
+	}
+
+	private CharSequence mapaAleatorio() {
+		List<String> listaMapas = Arrays.asList("Castillo",
+
+				"Dorado", "Ecopunto: Antártica", "Eichenwalde", "Hanamura", "Hollywood", "Ilios", "Industrias Volskaya",
+				"King's Row", "Necrópolis", "Nepal", "Numbani", "Observatorio: Gibraltar", "Oasis", "Ruta 66",
+				"Selva Negra", "Templo de Anubis", "Torre Lijiang");
+
+		return listaMapas.get(new Random().nextInt(listaMapas.size()));
+	}
+
+	private String crearAviso(MessageChannel canal) {
+
+		StringBuilder aviso = new StringBuilder();
+
+		aviso.append("@everyone\n");
+		aviso.append("\tEstás serán las reglas:\n");
+		aviso.append(
+				"\t-En Lobby se elegirá l@s lideres de 'Ñuñu Team' y 'CuckyTeam' (Se intentará equilibrar por habilidad);.\n");
+		aviso.append(
+				"\t- Será 5vs5 si estamos todos este día, depende como varie los equipos, cambiará el tiempo de reaparición.\n");
+		aviso.append(
+				"\t- Si hay jugadores impares (ejemplo 5vs6);, el equipo con desventaja puede Optar o no por un bot con el personaje que deseen (Con dificultad Normal);.\n");
+		aviso.append("\t- Se jugará un total de 4 partidos que compondrán los siguientes mapas:\n");
+		aviso.append("\t   · Asalto → París\n");
+		aviso.append("\t   · Asalto/Escolta → Blizzard World\n");
+		aviso.append("\t   · Control → Busan\n");
+		aviso.append("\t   · Escolta → Rialto\n");
+		aviso.append("\t(En caso de empate se decidirá si hacer un 5º mapa por votación popular);.\n");
+		aviso.append("\t- Empezará el torneo 'Ñuñus Cup Edición 2' dia 02/03/2019 a las 23:59.\n");
+		aviso.append("\t-------------------------------------------------------------------------\n");
+		aviso.append("\t¡MUCHA SUERTE Y PASADLO BIEN!");
+
+		return aviso.toString();
 
 	}
 
@@ -87,6 +128,9 @@ public class BotListener extends ListenerAdapter {
 
 	private String listarJugadores() {
 		StringBuilder participantes = new StringBuilder("Lista De Jugadores Del Torneo: \n");
+
+		listaJugadores.addAll(Arrays.asList("Maquina1995", "Ericmeca98", "Carlos", "MagonxESP", "Helena",
+				"HizoPOOMyaestaaquilaguerra", "Goliath", "Oliver", "Mikel", "Bir3venge"));
 
 		for (String jugador : listaJugadores) {
 			participantes.append(jugador + "\n");
